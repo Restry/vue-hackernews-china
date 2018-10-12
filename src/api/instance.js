d1361405130e5cd4576973cc259117af1c977a87
Louis <michael.wei@foxmail.com>
2018-10-12 10:59:59 +0800

    更新服务器地址

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,8 +1,29 @@
 import axios from 'axios';
 
+
+const getConfig = () => {
+  const port = process.env.PORT || '8080';
+
+  const config = {
+    prod: {
+      url: 'http://127.0.0.1:'+port+'/api',
+    },
+
+    dev: {
+      url: 'http://127.0.0.1:'+port+'/api',
+    }
+  }
+  if (process.env.NODE_ENV === 'development') {
+    return config.dev
+  } else {
+    return config.prod
+  }
+}
+
 export default (context) => {
+  const config =getConfig();
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8080/api',
+    baseURL: config.url,
     // withCredentials: true,
     headers: {
       'X-Custom-Header': context ? context.city.pinyin : ''
